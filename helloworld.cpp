136462284226cc786e4750f2c34c4096ef3e919d
tanish <tanisharora2312@gmail.com>
2022-03-23 19:47:58 +0530

    added new cpp file

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,12 +4,14 @@ using namespace std;
 void display()
 {
 	cout << "this is a simple display function" << endl;
-	
+
 
 }
 int main()
 {
 	cout << "hello world" << endl;
+	display();
+	display();
 	return 0;
 
 }
---
+++ marioworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "this is another c++ file" << endl;
+	return 0;
+	
+}
